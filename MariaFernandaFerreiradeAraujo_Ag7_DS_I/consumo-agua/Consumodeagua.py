f262810301fe8c00aff1cbdebc921f23fef93eb7
Maria Fernanda F. A. <mafezinha2008@gmail.com>
2026-09-13 16:25:55 -0300

    Update Consumodeagua.py

--- MariaFernandaFerreiradeAraujo_Ag7_DS_I/consumo-agua/Consumodeagua.py
+++ MariaFernandaFerreiradeAraujo_Ag7_DS_I/consumo-agua/Consumodeagua.py
@@ -1,7 +1,8 @@
+#Entrada das informações
 Imovel = input ("Informe o tipo do imovel (casa, comercial ou apartamento):")
 Consumo = float (input ("Informe o consumo mensal de agua do imovel (em m³):"))
 
-
+#Classificações
 if Imovel == "comercial":
    print("Tarifa comercial aplicada - consulte o plano corporativo.")
 
