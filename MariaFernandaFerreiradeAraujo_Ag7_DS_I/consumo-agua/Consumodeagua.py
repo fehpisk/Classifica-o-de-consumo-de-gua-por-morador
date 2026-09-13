80c3faddb368dc16e80b399a035d1a1e49393dba
Maria Fernanda F. A. <mafezinha2008@gmail.com>
2026-09-13 16:24:57 -0300

    Add files via upload

---
+++ MariaFernandaFerreiradeAraujo_Ag7_DS_I/consumo-agua/Consumodeagua.py
@@ -0,0 +1,21 @@
+Imovel = input ("Informe o tipo do imovel (casa, comercial ou apartamento):")
+Consumo = float (input ("Informe o consumo mensal de agua do imovel (em m³):"))
+
+
+if Imovel == "comercial":
+   print("Tarifa comercial aplicada - consulte o plano corporativo.")
+
+elif Imovel == "apartamento" and Consumo < 10:
+   print("Consumo econômico - excelente controle de água!")
+
+elif Imovel == "apartamento" and Consumo <= 25 or (Imovel == "casa" and Consumo <= 25):
+ print("Consumo moderado - dentro do padrão residencial.")
+
+else: 
+  print("Consumo excessivo - adote medidas de economia e verifique vazamentos.")
+
+
+
+
+
+
